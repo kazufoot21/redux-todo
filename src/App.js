405e06e670b7d0de051068268329a6b061f266f2
kazufoot21 <kazufoot21@outlook.jp>
2022-01-02 16:11:26 +0900

    list of complete incomplete

--- src/App.js
+++ src/App.js
@@ -6,11 +6,21 @@ function App() {
   return (
     <div className="App">
       <h1>Redux Todo リスト作成</h1>
-      <h2>Todo List</h2>
+      <h2>Incomplete Todo List</h2>
       <ul>
-        {lists.map((list, index) => (
-          <li key={index}>{list.name}</li>
-        ))}
+        {lists
+          .filter((list) => list.complete === false)
+          .map((list, index) => (
+            <li key={index}>{list.name}</li>
+          ))}
+      </ul>
+      <h2>Complete Todo List</h2>
+      <ul>
+        {lists
+          .filter((list) => list.complete === true)
+          .map((list, index) => (
+            <li key={index}>{list.name}</li>
+          ))}
       </ul>
     </div>
   );
