7def6ba20d7db2840d0ecdb187a36eefd494ca39
kazufoot21 <kazufoot21@outlook.jp>
2022-01-02 16:06:24 +0900

    access todo and show

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import { useSelector } from 'react-redux';
 
 function App() {
+  const lists = useSelector((state) => state.lists);
   return (
     <div className="App">
       <h1>Redux Todo リスト作成</h1>
+      <h2>Todo List</h2>
+      <ul>
+        {lists.map((list, index) => (
+          <li key={index}>{list.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
